3443018a3a9abea462c2ab97ad837c6b026120a4
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-13 20:05:01 +0100

    Added code to the router for creation.

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         return this.store.find('todo');
+    },
+
+    actions: {
+        createTodo: function(newTitle) {
+            var todo = this.store.createRecord('todo', {
+                title: newTitle,
+                isComplete: false
+            });
+
+            // Clear the 'New Todo' text field
+            this.controllerFor('todos').set(newTitle, '');
+
+            // Save the new model
+            todo.save();
+        }
     }
 });
